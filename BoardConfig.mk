b5c6ffcb17b567cc7aaf656a96456c1fd1fac8d5
necio Errante <necioerrante@gmail.com>
2015-03-28 15:41:31 -0400

    Clean Up

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,14 +27,13 @@ TARGET_NO_RADIOIMAGE := true
 TARGET_NO_RECOVERY := false
 TARGET_NO_KERNEL := false
 
-TARGET_NO_BOOTLOADER := true
+#TARGET_NO_BOOTLOADER := true
 
 BOARD_KERNEL_BASE        := 0x00000000
 BOARD_KERNEL_PAGESIZE    := 2048
 BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
 BOARD_RAMDISK_OFFSET     := 0x02000000
 
-# Test dtbTool and custombootimg.mk
 BOARD_KERNEL_BOOTIMG := true
 TARGET_NO_SEPARATE_RECOVERY := true
 BOARD_CUSTOM_BOOTIMG := true
@@ -102,26 +101,3 @@ WITH_DEXPREOPT := true
 # Recovery
 TARGET_RECOVERY_FSTAB := device/sony/shinano/rootdir/fstab.shinano
 TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
-
-# TWRP flags
-DEVICE_RESOLUTION := 720x1280
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
-RECOVERY_SDCARD_ON_DATA := true
-TW_HAS_NO_RECOVERY_PARTITION := true
-TW_FLASH_FROM_STORAGE := true
-TW_EXTERNAL_STORAGE_PATH := "/external_sd"
-TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
-TW_DEFAULT_EXTERNAL_STORAGE := true
-# TW_INCLUDE_CRYPTO := true
-TW_INCLUDE_JB_CRYPTO := true
-TW_CRYPTO_FS_TYPE := "ext4"
-TW_CRYPTO_REAL_BLKDEV := "/dev/block/platform/msm_sdcc.1/by-name/userdata"
-TW_CRYPTO_MNT_POINT := "/data"
-TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,barrier=1,noauto_da_alloc,discard"
-TW_CRYPTO_FS_FLAGS := "0x00000406"
-TW_CRYPTO_KEY_LOC := "footer"
-TW_INCLUDE_FUSE_EXFAT := true
-# TW_BOARD_CUSTOM_GRAPHICS := ../../../device/sony/shinano-common/recovery/twrpgraphics.c
-TW_BRIGHTNESS_PATH := /sys/class/leds/wled:backlight/brightness
-TW_MAX_BRIGHTNESS := 4095
-TW_NO_SCREEN_BLANK := true
